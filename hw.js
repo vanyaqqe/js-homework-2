a4a03329733d7680a7dea794aec66b274cc8f727
ivanl <162118702+vanyaqqe@users.noreply.github.com>
2024-07-16 14:03:46 +0200

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,58 @@
+// 2nd Homework
+
+// const fruits = [
+// { id: 0, name: "Apple" },
+// { id: 1, name: "Tomat" },
+// { id: 2, name: "Cherry" },
+// { id: 3, name: "Orange" },
+// ];
+
+// console.log(fruits.map(item => item.name));
+
+
+
+
+// for (let i = 2; i <= 10; i++){
+//     if (i % 2 === 1) continue
+//     console.log(i)
+// }
+
+
+
+// let i = 0;
+
+// do {
+//     console.log(`цифра ${i}!`);
+//     i++;
+// } while (i < 5);
+
+
+
+
+
+// let num;
+
+// do {
+//   num = prompt("Введіть число більше за 100");
+// } while (num <= 100 && num);
+
+
+
+
+// function getAverageAge(users) {
+//   return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+// }
+
+// const girls = [
+// { age: 23, name: "Оля" },
+// { age: 29, name: "Аня" },
+// { age: 10, name: "Юля" },
+// { age: 20, name: "Катя" },
+// ];
+
+// console.log( getAverageAge(girls) );
+
+
+
+
+
